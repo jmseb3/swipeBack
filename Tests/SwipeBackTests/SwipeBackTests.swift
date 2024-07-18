4ec82995652832e6857a0ab81ffba57b332d0b53
정원희 Service Development Team <wonhee.jung@creverse.com>
2024-07-18 11:48:04 +0900

    Initial Commit

---
+++ Tests/SwipeBackTests/SwipeBackTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SwipeBack
+
+final class SwipeBackTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
